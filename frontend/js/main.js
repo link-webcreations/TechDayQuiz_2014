2377adb725633c4f110919729fc040552def3437
Vincent BESANCON <besancon.vincent@gmail.com>
2014-09-26 11:04:46 +0200

    Jquery and bootstrap now loaded.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -10,25 +10,30 @@ requirejs.config({
     paths: {
         'angular': '../bower_components/angular/angular.min',
         'angular-route': '../bower_components/angular-route/angular-route.min',
-        'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
-        'jquery': '../bower_components/jquery/dist/jquery.min'
+        'jquery': '../bower_components/jquery/dist/jquery.min',
+        'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min'
     },
 
     // angular does not support AMD out of the box, put it in a shim
     // Checkout more about AMD: https://github.com/amdjs/amdjs-api/blob/master/AMD.md
     shim: {
+        'bootstrap': {
+            deps: ['jquery']
+        },
         'angular': {
             exports: 'angular'
         },
         'angular-route':{
-            deps:['angular'],
+            deps: ['angular']
         }
     }
 });
 
 // Start the application.
-require([
+requirejs([
+    'jquery',
+    'bootstrap',
     'app'
-], function (app) {
+], function ($, _bootstrap, app) {
   app.init();
 });
