821a3278170e258c5ee99cb4f98d83c5547a6885
Vincent BESANCON <besancon.vincent@gmail.com>
2014-10-01 19:45:54 +0200

    [frontend] renames Person factory to Participant.

--- frontend/js/modules/submit/controllers.js
+++ frontend/js/modules/submit/controllers.js
@@ -2,13 +2,13 @@ define(['angular'], function(angular) {
     'use strict';
 
     angular.module('quizApp.submit.controllers', ['quizApp.main.api'])
-        .controller('SubmitCtrl', ['$scope', 'Person', function($scope, Person) {
+        .controller('SubmitCtrl', ['$scope', 'Participant', function($scope, Participant) {
             $scope.submit_results = function() {
                 $scope.submit_errors = null;
                 $scope.submit_success = false;
 
                 if ($scope.submitForm.$valid) {
-                    Person.save($scope.person).$promise
+                    Participant.save($scope.participant).$promise
                         .then(
                             function(success) {
                                 $scope.submit_success = true;
