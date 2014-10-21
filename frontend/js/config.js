26f2846b964f61e9deb22ea5c84c1c00d06182ac
Yves ANDOLFATTO <yves.andolfatto@faurecia.com>
2014-10-21 16:47:47 +0200

    [frontend] fix backend API entry point.

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -3,7 +3,7 @@ define([], function() {
 
     return {
         // API entry point
-        'api_entry_point': '/backend/api',
+        'api_entry_point': '/backend/techday/api',
 
         // The directory where to find all partials
         'partials_dir': '/techday/partials'
