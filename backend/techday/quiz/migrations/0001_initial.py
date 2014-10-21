f08d72c67a5cca0208afad295140ca2a77ee0d89
Yves ANDOLFATTO <yves.andolfatto@faurecia.com>
2014-10-21 15:12:25 +0200

    [backend] update migrations.

--- backend/techday/quiz/migrations/0001_initial.py
+++ backend/techday/quiz/migrations/0001_initial.py
@@ -28,8 +28,8 @@ class Migration(migrations.Migration):
             name='Participant',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('firstname', models.CharField(max_length=255, blank=True)),
-                ('lastname', models.CharField(max_length=255, blank=True)),
+                ('firstname', models.CharField(max_length=255, null=True, blank=True)),
+                ('lastname', models.CharField(max_length=255, null=True, blank=True)),
                 ('email', models.EmailField(unique=True, max_length=254, validators=[quiz.validators.validate_email_from_faurecia])),
             ],
             options={
